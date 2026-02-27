bc633e6ebf2854a0bd373124e352660157f88502
Rajat-Srivastava-Work <Rajat.Srivastava@coforge.com>
2026-02-27 19:29:31 +0530

    Commited assignment files

--- Day4MavenPrj/src/main/java/com/coforge/Day4MavenPrj/DemoOnBuiltInFI.java
+++ Day4MavenPrj/src/main/java/com/coforge/Day4MavenPrj/DemoOnBuiltInFI.java
@@ -2,6 +2,7 @@ package com.coforge.Day4MavenPrj;
 
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 public class DemoOnBuiltInFI {
 	public static void main(String[] args) {
@@ -41,6 +42,21 @@ public class DemoOnBuiltInFI {
 		
 		System.out.println(isEven.apply(54));
 		
+		//Predicate
+		Predicate<Integer> checkEven = (num)->num%2==0;
+		System.out.println(checkEven.test(34));
+		
+		Predicate<String> chkLength=(str)->str.length()>5;
+		System.out.println(chkLength.test("Rajat"));
+		
+		Predicate<String> authenticateUsername = (uname)->uname.equals("Rajat");
+		Predicate<String> authenticatePassword=(pwd)->pwd.equals("Rajat123");
+		
+		String result = authenticateUsername.test("Rajat") && authenticatePassword.test("Rajat123") ? "Authentication successfull" : "Invalid Credentials";
+		System.out.println(result);
+		
+		Predicate<String> validateUsername = (uname) -> uname.length()>3 && !(uname.charAt(0)==' ') && uname!=null && !uname.isBlank();
+		System.out.println(validateUsername.test("Rajat"));
 	}
 
 }
