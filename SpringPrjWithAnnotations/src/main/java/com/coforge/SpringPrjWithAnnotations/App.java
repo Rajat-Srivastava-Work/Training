48435dd249601e0aedc4aea6bfab097363359878
Rajat-Srivastava-Work <Rajat.Srivastava@coforge.com>
2026-03-15 21:36:43 +0530

    day 16 new commit

--- SpringPrjWithAnnotations/src/main/java/com/coforge/SpringPrjWithAnnotations/App.java
+++ SpringPrjWithAnnotations/src/main/java/com/coforge/SpringPrjWithAnnotations/App.java
@@ -1,3 +1,4 @@
+
 package com.coforge.SpringPrjWithAnnotations;
 
 import org.springframework.context.ApplicationContext;
@@ -6,22 +7,37 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.coforge.Beans.Book;
 import com.coforge.Beans.Order;
+import com.coforge.Beans.Student;
 import com.coforge.configuration.BookOrderConfig;
+import com.coforge.configuration.StudentConfig;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+//        ApplicationContext context = new ClassPathXmlApplicationContext("book-bean.xml");
+//        Book book = (Book) context.getBean("book");
+//        System.out.println(book);
+//        
+//        Order ord = (Order) context.getBean("ord");
+//        System.out.println(ord);
+    	
+//      ApplicationContext context = new AnnotationConfigApplicationContext(BookOrderConfig.class);
+//      Book book = (Book) context.getBean("book");
+//      System.out.println(book);
+//      
+//      Order ord = (Order) context.getBean("ord");
+//      System.out.println(ord);
+    	
+    	ApplicationContext context = new AnnotationConfigApplicationContext(StudentConfig.class);
+        Student stud = (Student) context.getBean("std");
+        System.out.println(stud);
         
-        //ApplicationContext context=new ClassPathXmlApplicationContext("book-bean.xml");
-        ApplicationContext context=new AnnotationConfigApplicationContext(BookOrderConfig.class);
-
-        Book b=(Book) context.getBean("book");
-        System.out.println(b);
+        Student stud1 = (Student) context.getBean("std");
+        System.out.println(stud1);
         
-        Order o=(Order) context.getBean("order");
-        System.out.println(o);
+        System.out.println(stud.hashCode());
+        System.out.println(stud1.hashCode());
     }
 }
